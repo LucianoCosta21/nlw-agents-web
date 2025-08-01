aad00b831728dba0286ae443d849803a3642436a
Luciano Costa <lucianocosta21@hotmail.com>
2025-08-01 14:52:23 -0300

    feat: create rooms and room UI

---
+++ src/components/question-form.tsx
@@ -0,0 +1,85 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod/v4';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from './ui/card';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form';
+import { Textarea } from './ui/textarea';
+import { Button } from './ui/button';
+import { useForm } from 'react-hook-form';
+
+const createQuesttionSchema = z.object({
+  question: z
+    .string()
+    .min(1, 'Pergunta é obrigatória')
+    .min(10, 'Pergunta deve ter pelo menos 10 caracteres')
+    .max(500, 'Pergunta deve ter menos de 500 caracteres'),
+});
+
+type CreateQuestionFormData = z.infer<typeof createQuesttionSchema>;
+
+interface QuestionFormProps {
+  roomId: string;
+}
+
+export function QuestionForm({ roomId }: QuestionFormProps) {
+  const form = useForm<CreateQuestionFormData>({
+    resolver: zodResolver(createQuesttionSchema),
+    defaultValues: {
+      question: '',
+    },
+  });
+
+  function handleCreateQuestion(data: CreateQuestionFormData) {
+    console.log(data, roomId);
+  }
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Fazer uma Pergunta</CardTitle>
+        <CardDescription>
+          Digite sua pergunta abaixo para receber uma resposta gerada por I.A.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form
+            className="flex flex-col gap-4"
+            onSubmit={form.handleSubmit(handleCreateQuestion)}
+          >
+            <FormField
+              control={form.control}
+              name="question"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Sua Pergunta</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      className="min-h-[100px]"
+                      placeholder="O que você gostaria de saber?"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Enviar Pergunta</Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
+}
